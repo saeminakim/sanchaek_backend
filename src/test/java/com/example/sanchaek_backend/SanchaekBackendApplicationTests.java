2171ef789192a82413d9cdf8a227d94f739c789d
Lily <lilykim@globalorder.co.kr>
2021-08-03 14:06:53 +0900

    initial commit

---
+++ src/test/java/com/example/sanchaek_backend/SanchaekBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sanchaek_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanchaekBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
