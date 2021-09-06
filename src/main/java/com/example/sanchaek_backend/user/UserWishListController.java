1a3035d268fca4f4a30311da95398541949859fb
Lily <helloimlily@gmail.com>
2021-09-06 21:57:01 +0900

    0906 WishList

--- src/main/java/com/example/sanchaek_backend/user/UserWishListController.java
+++ src/main/java/com/example/sanchaek_backend/user/UserWishListController.java
@@ -1,7 +1,6 @@
 package com.example.sanchaek_backend.user;
 
 import com.example.sanchaek_backend.book.Book;
-import com.example.sanchaek_backend.book.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,23 +11,21 @@ import java.util.List;
 
 @RestController
 public class UserWishListController {
-    private WishBookRepository repo;
+    private WishBookRepository bookRepo;
 
     @Autowired
     UserWishListController(WishBookRepository repo) {
-        this.repo = repo;
+        this.bookRepo = repo;
     }
 
     @GetMapping("/wishlist")
     public List<Book> getWishList() {
-        return repo.findAll();
+        return bookRepo.findAll();
     }
 
     @PostMapping("/wishlist")
     public void AddWishList(@RequestBody Book book) {
         book.setSaved(true);
-//        book.setIsRead(false);
-        System.out.println(book.toString());
-        repo.save(book);
+        bookRepo.save(book);
     }
 }
